3499f97233a5740a9f9b9196033fd6ccfc9db313
hikmahDikko <81673577+hikmahDikko@users.noreply.github.com>
2022-07-18 20:09:43 +0000

    Fizz or Buzz

---
+++ fizz_Buzz.js
@@ -0,0 +1,25 @@
+//Program to return out every integer from 1 to the number
+
+let number = 10;
+
+function decNum(a){
+  for(let i = 1; i <= a; i++){
+    console.log(i);
+  }
+}
+
+decNum(number);
+
+//Program to return if a number is either fizz or buzz
+
+function modTwo(b){
+  if(b % 2 == 0 && b % 3 == 0){
+    console.log("Fizz Buzz");
+  }else if(b % 2 == 0){
+    console.log("Fizz");
+  }else if(b % 3 == 0){
+    console.log("Buzz");
+  }
+}
+
+modTwo(number);
