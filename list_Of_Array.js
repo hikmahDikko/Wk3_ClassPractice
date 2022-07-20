7351bcee7a02040bca2c86fd4556edfe0dafb848
hikmahDikko <81673577+hikmahDikko@users.noreply.github.com>
2022-07-20 12:04:20 +0000

    List of Array Formation

---
+++ list_Of_Array.js
@@ -0,0 +1,11 @@
+let arr = Number(3);
+
+
+let listArr = new Array(arr);
+//listArr(arr);
+for(let j = 0; j < listArr.length; j++){
+  console.log("The ArrayList formed at index " + j + " are as follows;");
+  for(let i = 0; i < listArr.length; i++){
+    console.log("Array of index " + i);
+  }
+}
