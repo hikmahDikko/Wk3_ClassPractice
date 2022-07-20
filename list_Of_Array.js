6fc32a683b3df9b2abac050d93fc8dc7f7e65a06
hikmahDikko <81673577+hikmahDikko@users.noreply.github.com>
2022-07-20 12:20:28 +0000

    list_Of_Array

--- list_Of_Array.js
+++ list_Of_Array.js
@@ -6,6 +6,6 @@ let listArr = new Array(arr);
 for(let j = 0; j < listArr.length; j++){
   console.log("The ArrayList formed at index " + j + " are as follows;");
   for(let i = 1; i <= listArr.length; i++){
-    console.log("Array of index " + i);
+    console.log("Array value is " + i);
   }
 }
