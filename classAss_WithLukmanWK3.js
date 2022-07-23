3adbb05223ff259dfcb66f3a46d22fc23e1dcdc5
hikmahDikko <81673577+hikmahDikko@users.noreply.github.com>
2022-07-23 12:37:28 +0000

    classAss_WithLukmanWK3

--- classAss_WithLukmanWK3.js
+++ classAss_WithLukmanWK3.js
@@ -2,25 +2,26 @@
 let word = "hello";
 
 //function to swap the word
-function wordSwap(a){
-    //A variable to get the length of the word
-    let u = a.length - 2;
+function wordSwap(a) {
+  //A variable to get the length of the word
+  let u = a.length - 2;
 
-    //Variables to store some values (i.e. to help save some strings)
-    let x = '';
-    let y = '';
+  //Variables to store some values (i.e. to help save some strings)
+  let x = '';
+  let y = '';
 
-    // Looping through the word to get a string at a particular index
-    for(let j = u; j < a.length; j++){
-        x += a[j] + '';
-    }
-        
-    for(let i = 0; i < a.length-2; i++){
-        y += a[i] + '';
-    }
-    
-    //To print the word generated
-        console.log(x + y);
+  // Looping through the word to get a string at a particular index
+  for (let j = u; j < a.length; j++) {
+    x += a[j] + '';
+  }
+
+  for (let i = 0; i < a.length - 2; i++) {
+    y += a[i] + '';
+  }
+
+  //To print the word generated
+  return (x + y);
 }
 
-wordSwap(word);
+let swap = wordSwap(word);
+console.log(word + " swapped to " + swap);
